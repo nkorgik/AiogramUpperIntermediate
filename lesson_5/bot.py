b7c7159cbe4f78b1d0dccc89d009867e2c8439b9
Almaz Khusnutdinov <almazkhusnutdinov@Almazs-MacBook-Air.local>
2022-12-31 14:12:19 -0800

    first_video_commit_2

--- lesson_5/bot.py
+++ lesson_5/bot.py
@@ -1,15 +1,19 @@
-from aiogram import Dispatcher, executor, types, Bot
-from tk import TOKEN_API
+from aiogram import Dispatcher, executor, types, Bot # импорт основных элементов библиотеки
+# Dispatcher - класс для создания диспетчера, отслеживающего обновления
+# executor - объект по исполнению каких-либо задач
+# types - модуль всех типов данных библиотеки
+# Bot - класс для создания экземпляра (сущности) бота
+from tk import TOKEN_API # импортирование токена
 
 
-bot = Bot(token=TOKEN_API)
-dp = Dispatcher(bot=bot)
+bot = Bot(token=TOKEN_API) # создание экземпляра бота
+dp = Dispatcher(bot=bot) # создание экземпляра диспетчера
 
 
-@dp.message_handler()
-async def count_symbols(msg: types.Message) -> None:
-    await msg.answer(len(msg.text) - msg.text.count(' '))
+@dp.message_handler() # получение объекта декоратора
+async def count_symbols(msg: types.Message) -> None: # объявление функции обработчика
+    await msg.answer(len(msg.text) - msg.text.count(' ')) # ответ пользователю, где msg - объект сообщения, которое он нам отправил
 
 
-if __name__ == "__main__":
-    executor.start_polling(dispatcher=dp)
+if __name__ == "__main__": # классическая идиома языка питон для непосредственного запуска модуля
+    executor.start_polling(dispatcher=dp) # запуск бота в режиме полинга
--- lesson_5/tk.py
+++ lesson_5/tk.py
@@ -1,1 +1,3 @@
+# you dont have to expose this token!!
+
 TOKEN_API = "5874953737:AAER7xB3HBHm82IOCXFUxxFcR5GNpxuOHLg"
